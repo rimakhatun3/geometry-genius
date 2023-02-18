dd08b47dbc81cfa5b9955b811a922573a817e9e0
rimakhatun3 <rimamnt46@gmail.com>
2023-02-18 11:27:54 +0600

    added triangle and rectangle function total

--- js/geometry.js
+++ js/geometry.js
@@ -27,15 +27,44 @@ document.getElementById('btn-rectangle').addEventListener('click',function(){
     const rectangText = document.getElementById('rectangle-text').innerText;
     const firstRectangleInput = getInputById('first-rectangle-input');
     const sceondRectangleInput = getInputById('sceond-rectangle-input');
-const btn = document.getElementById('convert');
     const total =  firstRectangleInput * sceondRectangleInput;
 
-   getData(rectangText,total,btn);
+   getData(rectangText,total);
+  
+})
+document.getElementById('btn-paralogram').addEventListener('click',function(){
+    serial += 1
+    const paraloText = document.getElementById('paralo-text').innerText;
+    const firstParaloInput = getInputById('first-paralo-input');
+    const sceondParaloInput = getInputById('sceond-paralo-input');
+    const total =  firstParaloInput * sceondParaloInput;
+
+   getData(paraloText,total);
+  
+})
+document.getElementById('btn-rhombus').addEventListener('click',function(){
+    serial += 1
+    const rhombusText = document.getElementById('rhombus-text').innerText;
+    const firstRhombusInput = getInputById('first-rhombus-input');
+    const sceondRhombusInput = getInputById('sceond-rhombus-input');
+    const total =  0.5*firstRhombusInput* sceondRhombusInput;
+
+   getData(rhombusText,total);
+  
+})
+document.getElementById('btn-pentagon').addEventListener('click',function(){
+    serial += 1
+    const pentagonText = document.getElementById('pentagon-text').innerText;
+    const firstPentagonInput = getInputById('first-pentagon-input');
+    const sceondPentagonInput = getInputById('sceond-pentagon-input');
+    const total =  0.5*firstPentagonInput* sceondPentagonInput;
+
+   getData(pentagonText,total);
   
 })
 
 
-function getData(productName,total,bt){
+function getData(productName,total){
     const container = document.getElementById('table-container');
 //     const btn = document.createElement("button");
 // btn.innerHTML = "convert to m2";
@@ -43,8 +72,8 @@ function getData(productName,total,bt){
 const tr = document.createElement('tr');
 tr.innerHTML =`<td>${serial}</td>
 <td>${productName}</td>
-<td>${total}</td>
-<td>${bt}</td>
+<td>${total}cm <sup>2</sup></td>
+<td><button id="convert" class="px-8 py-2 bg-blue-600 ">Convert To m<sup>2</sup></button></td>
 `;
 container.appendChild(tr);
 
